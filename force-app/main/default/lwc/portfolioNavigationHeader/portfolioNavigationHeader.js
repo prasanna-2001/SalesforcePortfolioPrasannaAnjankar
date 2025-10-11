e32009bdfba2a99254f124a32141d5a06e4a2326
Prasanna Anjankar <prasannaanjankar@gmail.com>
2025-10-11 19:40:37 +0530

    My Portfolio main skeleton and Navigation Component Added

---
+++ force-app/main/default/lwc/portfolioNavigationHeader/portfolioNavigationHeader.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+//import PortfolioAssets from "@salesforce/resourceUrl/PortfolioAssets";
+
+export default class PortfolioNavigationHeader extends LightningElement {
+    // navicon = PortfolioAssets + '/resource/navbar.png';
+    // flag = true;
+
+
+
+}
