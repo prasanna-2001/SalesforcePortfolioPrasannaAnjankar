b30ea3c1149cfe1f01cca264587ce7e1c0c94f37
Prasanna Anjankar <prasannaanjankar@gmail.com>
2025-11-02 11:03:48 +0530

    Updated experience Section

--- force-app/main/default/lwc/portfolioExperienceSection/portfolioExperienceSection.js
+++ force-app/main/default/lwc/portfolioExperienceSection/portfolioExperienceSection.js
@@ -1,3 +1,11 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
+import PortfolioAssets from "@salesforce/resourceUrl/PortfolioAssets";
 
-export default class PortfolioExperienceSection extends LightningElement {}
+export default class PortfolioExperienceSection extends LightningElement {
+    @api experiences;
+    persiLogo = PortfolioAssets + '/PortfolioAssets/persiLogo.png';
+    persiUrl = 'https://www.persistent.com/';
+    connectedCallback() {
+        console.log('inside project: ' + this.experiences);
+    }
+}
